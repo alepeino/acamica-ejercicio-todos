0e23bd9980db72c975d0b1a9b335ef915a6ebf5f
Alejandro Peinó <alepeino@gmail.com>
2019-11-20 18:55:54 -0300

    reordena funciones

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,7 @@ const todosTemplate = document.getElementById('todo-template')
 const todosList = document.getElementById('todo-list')
 
 function listarTodos () {
-
-}
-
-function actualizarLista(todos) {
-  todosList.innerHTML = ejs.render(todosTemplate.text, { todos })
+  //...
 }
 
 function agregarTodo (descripcion) {
@@ -15,4 +11,8 @@ function agregarTodo (descripcion) {
 
 function marcarComoHecho (todo) {
   //...
+}
+
+function actualizarLista(todos) {
+  todosList.innerHTML = ejs.render(todosTemplate.text, { todos })
 }
